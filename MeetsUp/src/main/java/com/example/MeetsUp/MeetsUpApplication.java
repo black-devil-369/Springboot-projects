9bf7e67324790219f7cb1cede10375743bf781b9
Ajay Chauhan <92268333+black-devil-369@users.noreply.github.com>
2024-11-18 12:40:47 +0000

    this file is used to start our program

---
+++ MeetsUp/src/main/java/com/example/MeetsUp/MeetsUpApplication.java
@@ -0,0 +1,13 @@
+package com.example.MeetsUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetsUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetsUpApplication.class, args);
+	}
+
+}
